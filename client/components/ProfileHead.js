3b158de4b259723c5439ba9f13c75882c1d1966c
Hack Reactor Student <student@hackreactor.com>
2017-08-09 17:35:33 -0700

    Resolve conflicts

--- client/components/ProfileHead.js
+++ client/components/ProfileHead.js
@@ -5,17 +5,37 @@ export default class ProfileHead extends React.Component {
     super(props);
   }
 
+  // render() {
+  //   return (
+  //     <div className="profileHead">
+  //       <div className="profileImg">
+  //         <img src={this.props.data.profilepic} width="200" height="200"/>
+  //       </div>
+  //       <div className="profileInfo">
+  //         <h3>{this.props.data.firstname}, {this.props.data.age}</h3>
+  //         <p>{this.props.data.bio}</p>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   render() {
     return (
-      <div className="profileHead">
-        <div className="profileImg">
-          <img src={this.props.data.profilepic} width="200" height="200"/>
-        </div>
-        <div className="profileInfo">
-          <h3>{this.props.data.firstname}, {this.props.data.age}</h3>
-          <p>{this.props.data.bio}</p>
+      <div>
+        <div className="profileCard">
+          <img src={this.props.data.profilepic} width="300" height="200"/>
+          <div className="profileContainer">
+            <h2>John Doe</h2>
+            <p className="profileTitle">CEO and Founder, Example</p>
+            <p>Harvard University</p>
+            {/* <a className="profileButton" href="#"><i className="fa fa-dribbble"></i></a> 
+            <a className="profileButton" href="#"><i className="fa fa-twitter"></i></a> 
+            <a className="profileButton" href="#"><i className="fa fa-linkedin"></i></a> 
+            <a className="profileButton" href="#"><i className="fa fa-facebook"></i></a>  */}
+            <p><button className="cardButton">Match</button></p>
+          </div>
         </div>
       </div>
     );
   }
 }
+
