3cab72ab5478a402faf2ff93a0e7527b22acf6ee
Michael Griffin <michaelgriffin@Michaels-MacBook-Air.local>
2017-08-09 17:47:27 -0700

    added onclick to matches

--- client/components/Match.js
+++ client/components/Match.js
@@ -5,10 +5,12 @@ export default class Match extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
+			id: 0,
 			firstname: '',
 			age: 25,
 			profilepic: ''
 		}
+		this.renderProfile = this.renderProfile.bind(this);
 	}
 
 	componentDidMount(){
@@ -16,6 +18,7 @@ export default class Match extends React.Component{
 		.then(res => {
 			//console.log('This is a response from the db ', res.data.age);
 			this.setState({
+				id: res.data.id,
 				firstname: res.data.firstname,
 				age: res.data.age,
 				profilepic: res.data.profilepic
@@ -26,10 +29,16 @@ export default class Match extends React.Component{
 		})
 	}
 
+	renderProfile(){
+		console.log('This is rendering');
+		//axios.get(`api/profile/${this.state.id}`)
+
+	}
+
 	render(){
 		//console.log('This is the props in Match: ', this.props.match);
 		return (
-			<div className="col-sm-4 text-center match">
+			<div className="col-sm-4 text-center match" onClick={this.renderProfile}>
 				<h2>{this.state.firstname}, {this.state.age}</h2>
 				<img src={this.state.profilepic} className="match-pic"/>
 				<h3>{Math.round(100 * this.props.match.confidence + 10)}% Match</h3>
