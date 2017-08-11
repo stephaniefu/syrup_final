28bc6d31a771f2da6edeea838e29d9bfb78bc2d6
Stephanie Fu <StephaniefuU@gmail.com>
2017-08-11 16:22:20 -0400

    merged jr and steph db files

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-// const db_URL = require('dotenv').config();
+const url = require('../dbUrl');
 
-const db = new Sequelize('postgres://kopordan:93HHSxj-wO4nxzS3P1NANXGrdmtT0Whd@stampy.db.elephantsql.com:5432/kopordan', {
+const db = new Sequelize(url, {
   pool: {
     max: 3,
     min: 0,
